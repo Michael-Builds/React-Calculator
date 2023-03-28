012fec1b474e09ba7abf2126271639fea9e5410a
Kabanda Kpanti Michael <you@example.com>
2023-03-28 19:02:30 +0000

    Calculator App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [results, setResults] = useState("");
+
+  const clickhandler = (event) => {
+    setResults(results.concat(event.target.value));
+  };
+
+  const cleardisplay = () => {
+    setResults("");
+  };
+  const calculate = () => {
+    let newResults;
+    try {
+      newResults = eval(results);
+    } catch (error) {
+      console.error(error);
+      newResults = undefined;
+    }
+  
+    if (newResults !== undefined) {
+      setResults(newResults.toString());
+    } else {
+      setResults("");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="cal">
+      <input type="text" placeholder="0" id="answer" value={results} />
+      <input type="button" value="9" className="button" onClick = {clickhandler} />
+      <input type="button" value="8" className="button" onClick = {clickhandler} />
+      <input type="button" value="7" className="button" onClick = {clickhandler} />
+      <input type="button" value="6" className="button" onClick = {clickhandler} />
+      <input type="button" value="5" className="button"  onClick = {clickhandler}/>
+      <input type="button" value="4" className="button"  onClick = {clickhandler}/>
+      <input type="button" value="3" className="button" onClick = {clickhandler} />
+      <input type="button" value="2" className="button"  onClick = {clickhandler}/>
+      <input type="button" value="1" className="button" onClick = {clickhandler} />
+
+      <input type="button" value="0" className="button"  onClick = {clickhandler}/>
+      <input type="button" value="+" className="button"  onClick = {clickhandler}/>
+      <input type="button" value="-" className="button"  onClick = {clickhandler}/>
+      <input type="button" value="/" className="button" onClick = {clickhandler} />
+      <input type="button" value="*" className="button" onClick = {clickhandler} />
+      <input type="button" value="%" className="button" onClick = {clickhandler} />
+      <input type="button" value="." className="button"  onClick = {clickhandler}/>
+
+      <input type="button" value="Clear" className="button button1" onClick = {cleardisplay} />
+      <input type="button" value="=" className="button button1" onClick = {calculate} />
     </div>
   );
 }
